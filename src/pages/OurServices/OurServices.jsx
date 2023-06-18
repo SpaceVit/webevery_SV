c8286363def223a4eea22d8cf893ef584ea5c306
Vitaliy Pavlovskiy <sledozar@gmail.com>
2023-06-18 18:25:33 +0300

    once_again

--- src/pages/OurServices/OurServices.jsx
+++ src/pages/OurServices/OurServices.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './OurServices.module.scss';
-import { NavLink, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
 import SliderBar from 'components/share/SliderBar/SliderBar';
 
@@ -11,25 +11,6 @@ const OurServices = () => {
         <h1 className={styles.title}>Our Services</h1>
         <aside className={styles.aside}>
           <SliderBar />
-          {/* <ul className={styles.servicesLinksList}>
-            <li className={styles.servicesListItem}>
-              <NavLink to="consultation" className={styles.consultationLink}>
-                Consultation
-              </NavLink>
-            </li>
-
-            <li className={styles.servicesListItem}>
-              <NavLink to="webDeveloping" className={styles.consultationLink}>
-                Web-developing
-              </NavLink>
-            </li>
-
-            <li className={styles.servicesListItem}>
-              <NavLink to="supporting" className={styles.consultationLink}>
-                Supporting
-              </NavLink>
-            </li>
-          </ul> */}
         </aside>
         <Outlet></Outlet>
       </section>
