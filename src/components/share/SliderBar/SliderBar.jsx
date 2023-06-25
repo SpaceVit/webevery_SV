a1d785589cd6b5e55991d6cd545dfdadd05e6fdd
Maksimchik36 <Peltek1985@gmail.com>
2023-06-25 23:30:50 +0300

    fixed slider

--- src/components/share/SliderBar/SliderBar.jsx
+++ src/components/share/SliderBar/SliderBar.jsx
@@ -8,45 +8,40 @@ import styles from './SliderBar.module.scss';
 import dataSlider from './dataSlider';
 
 const SliderBar = () => {
-  const [slideIndex, setSlideIndex] = useState(1);
+  const [slideIndex, setSlideIndex] = useState(0);
   // const [linkTo, setLinkTo] = useState('')
 
-  const moveIndex = index => setSlideIndex(index);
+  // const moveIndex = index => setSlideIndex(index);
 
-  const rightSlide = index => {
-    moveIndex(index);
-    if (slideIndex !== dataSlider.length) {
+  const rightSlide = () => {
+    if (slideIndex !== dataSlider.length - 1) {
       setSlideIndex(slideIndex + 1);
-    } else if (slideIndex === dataSlider.length) {
-      setSlideIndex(1);
-    }
+    } else setSlideIndex(0);
   };
 
   const leftSlide = () => {
-    if (slideIndex !== 1) {
+    if (slideIndex !== 0) {
       setSlideIndex(slideIndex - 1);
-    } else if (slideIndex === 1) {
-      setSlideIndex(dataSlider.length);
-    }
+    } else setSlideIndex(dataSlider.length - 1);
   };
 
-  console.log(slideIndex);
+  // console.log(slideIndex);
   return (
     <div className={styles.sliderContainer}>
       {dataSlider.map((obj, index) => {
-        console.log('slider linkTo', obj.linkTo);
+        // console.log('slider linkTo', obj.linkTo);
         return (
-          <div key={index} className={slideIndex === index + 1 ? styles.activeDiv : styles.unactiveDiv}>
-            <NavLink onClick={() => leftSlide()} to={obj.linkTo} className={slideIndex === index + 1 ? styles.sliderActiveBtn : styles.sliderUnactiveBtn}>
+          <div key={index} className={slideIndex === index ? styles.activeDiv : styles.unactiveDiv}>
+            <NavLink onClick={() => leftSlide()} to={obj.linkPrev} className={slideIndex === index ? styles.sliderActiveBtn : styles.sliderUnactiveBtn}>
               <GoArrowLeft />
             </NavLink>
-            <NavLink to={obj.linkTo} className={slideIndex === index + 1 ? styles.slideActiveLink : styles.slideLink}>
+            <NavLink to={obj.linkTo} className={slideIndex === index ? styles.slideActiveLink : styles.slideLink}>
               {obj.linkText}
             </NavLink>
             <NavLink
-              onClick={() => rightSlide(index + 1)}
-              to={obj.linkTo}
-              className={slideIndex === index + 1 ? styles.sliderActiveBtn : styles.sliderUnactiveBtn}
+              onClick={() => rightSlide()}
+              to={obj.linkNext}
+              className={slideIndex === index ? styles.sliderActiveBtn : styles.sliderUnactiveBtn}
             >
               <GoArrowRight />
             </NavLink>
